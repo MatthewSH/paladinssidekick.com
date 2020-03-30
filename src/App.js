958de4b8d738dc6ce20e3e3723f61edc020d8c58
Matthew Hatcher <matthewh@halfpetal.com>
2020-03-30 17:10:15 -0400

    React app and site base.

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          <img src="https://static.paladinssidekick.com/logo.png" />
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Invite Paladins Sidekick to your Discord server.
+        </a>
+        <br/>
+        <small>Full website and documentation coming soon.</small>
+      </header>
+    </div>
+  );
+}
+
+export default App;
