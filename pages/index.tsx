e17b37c8554fa86afa896e479255c6a078812524
Matthew Hatcher <matthewh@halfpetal.com>
2020-04-11 16:22:57 -0400

    Fixing reference.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import Layout from '../components/layout';
-import Jumbotron from 'react-bootstrap/jumbotron';
+import Jumbotron from 'react-bootstrap/Jumbotron';
 import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
